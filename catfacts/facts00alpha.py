8e49ea8c3a32fb81c7522bb9812e3066658c869d
Stephen Trewick <stephent818@gmail.com>
2020-09-24 22:31:05 +0000

    contructors

--- catfacts/facts00alpha.py
+++ catfacts/facts00alpha.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python3
-"""Alta3 Research | Author: RZFeeser@alta3.com"""
 
-# imports always go at the top of your code
 import requests
+import random
 
 def main():
-    """Run time code"""
-    # create r, which is our request object
     r = requests.get("https://cat-fact.herokuapp.com/facts")
+    
+    chad_fact_list = []
+    
+    
 
-    # display the methods available to our new object
-    print( )
+    substr = "A cat"
+    for cat in r.json()["all"]:
+        old_fact = cat.get("text")
+        if old_fact.find(substr) != -1:
+            # replace the string "A cat" with a new substring
+            new_fact = old_fact.replace(substr, "Chad")
+            chad_fact_list.append(new_fact)
+
+    for fact in chad_fact_list:
+        print(fact)
+
+    
+
 main()
 
