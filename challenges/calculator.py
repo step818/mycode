678fda53b7935aa144411534e5ff3b4aea004416
Stephen Trewick <stephent818@gmail.com>
2020-09-24 15:45:12 +0000

    try/except added

--- challenges/calculator.py
+++ challenges/calculator.py
@@ -1,27 +1,30 @@
 #!/usr/bin/env python3
 
 def calculator():
-    x = float(input("Enter in a number"))
-    y= float(input("Enter ANOTHER  number"))
+    try:
+        x = float(input("Enter in a number: "))
+        y= float(input("Enter ANOTHER  number: "))
+    except:
+        print("Invalid input, try again.")
+        calculator()
+
     operation = ""
-
-
     while(operation != "add" and operation != "subtract" and operation != "divide" and operation != "multiply"): 
         
-         operation = input("What operation would like to perform? OPTIONS: 'add', 'subtract','divide','multiply'").lower()
-
-         if(operation == "add"):
-              add(x, y)
-         elif(operation == "subtract"):
-              subtract(x, y)
-         elif(operation == "divide"):
-              divide(x, y)
-         elif(operation == "multiply"):
-              multiply(x, y)
-         else:
-            print("use a valid OPTION")
-            continue
     
+     operation = input("What operation would like to perform? OPTIONS: 'add', 'subtract','divide','multiply': ").lower()
+    if(operation == "add"):
+        add(x, y)
+    elif(operation == "subtract"):
+        subtract(x, y)
+    elif(operation == "divide"):
+        divide(x, y)
+    elif(operation == "multiply"):
+        multiply(x, y)
+    else:
+        print("use a valid OPTION")
+        #continue
+     
 
 
 def add(x, y):
