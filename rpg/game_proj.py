29f2485f95c6c90673e955f8041ee1dc36531031
Stephen Trewick <stephent818@gmail.com>
2020-10-02 06:09:48 +0000

    some bugs in rpg

--- rpg/game_proj.py
+++ rpg/game_proj.py
@@ -15,6 +15,7 @@ This is an RPG addition to my instructors RPG for learning purposes
 import os
 import threading
 import neows02
+import time
 import game_input
 
 def showIntro():
@@ -65,34 +66,34 @@ def describeNoticeable():
 #an inventory, which is initially empty
 inventory = []
 
-#a dictionary linking a room to other rooms
-rooms = {
+rooms= {     
             'Foyer' : { 
                   'south' : 'Gatehouse',
                   'north' : 'Dining Room',
-                  'west' : 'Hall',
+                  'west' : 'Great Hall',
                   'noticeables': ['knight armor'],
                   'secrets': ['key']
                 },
-            'Hall' : {
+            'Great Hall' : {
                   'north' : 'Kitchen',
+                  'east' : 'Foyer',
                   'west' : 'Staircase (floor 1)'
                 },
             'Staircase (floor 1)' : {
-                'up' : 'Bottlery',
-                'down' : 'Dungeon'
+                'up' : 'Staircase (floor 2)',
+                'down' : 'Staircase (floor 0)',
+                'east' : 'Great Hall'
                 },
-            'Bottlery' : {
-                'down' : 'Hall',
-                'north': 'Buttery',
-                'south': 'Solar'
+            'Staircase (floor 2)' : {
+                'down' : 'Staircase (floor 1)',
+                'east' : 'Bed Chambers',
                 },
-            'Buttery' : {
-                'south': 'Bottlery',
-                'east': 'Bed Chambers'
+            'Bed Chambers' : {
+                'south' : 'Solar',
+                'west' : 'Staircase (floor 2)'
                 },
             'Solar' : {
-                'north': 'Bottlery',
+                'north': 'Bed Chambers',
                 'west': 'Wardrobe',
                 'noticeables' : ['crystal ball']
                 },
@@ -105,16 +106,18 @@ rooms = {
                 'noticeables' : ['lock']
                 },
             'Oubliette' : {
+                'north' : 'Dungeon',
                 'south' : 'Home'
                 },
             'Home' : {
                 'game' : 'winner'
                 },
             'Staircase (floor 0)' : {
-                'up' : 'Hall'
+                'up' : 'Staircase (floor1)',
+                'east' : 'Dungeon'
                 },
             'Kitchen' : {
-                'south' : 'Hall',
+                'south' : 'Great Hall',
                 'east' : 'Dining Room'
                 },
             'Dining Room' : {
@@ -145,7 +148,10 @@ rooms = {
          }
 
 # declare all the locked doors
-lockedRooms = ['Gallery', 'Solar', 'Oubliette']
+lockedRooms = ['Gallery', 'Solar', 'Oubliette', 'Home']
+
+# declare all monsters
+livingMonsters = ['Ancient Red Dragon']
 
 #start the player in the Hall
 currentRoom = 'Foyer'
@@ -172,7 +178,7 @@ while True:
   # split allows an items to have a space on them
   # get golden key is returned ["get", "golden key"]          
   move = move.lower().split(" ", 1)
-    
+  
   #if they type 'go' first
   if move[0] == 'go':
     if (move[1] in rooms[currentRoom]):
@@ -181,9 +187,9 @@ while True:
       if nextRoom in lockedRooms:
         print('The door to the room you\'re trying to open is locked')
         repeat = False
-      elif currentRoom == 'Oubliette' and 'Ancient Red Dragon' in livingMonsters:
-        print("That way is being blocked by the dragon.")
-        repeat = False
+      elif 'Ancient Red Dragon' in livingMonsters and nextRoom == 'Oubliette':
+        print(f"As you head in to the {nextRoom}, you see an Ancient Red Dragon straight ahead of you!!")
+        currentRoom = nextRoom
       else:
         #set the current room to the new room
         currentRoom = nextRoom
@@ -194,13 +200,15 @@ while True:
   
 
 
+
   # if they type 'inspect' first
   if move[0] == 'inspect' :
     if "noticeables" in rooms[currentRoom] and move[1] in rooms[currentRoom]['noticeables']:
       # Check if the Lock is being inspected
+      # And that the Oubliette hasnt been granted access already
       if (move[1] == 'lock'):
         password = input(f"Passcode: \n")
-        if (password == '2009KK8'):
+        if (password == '2009KK8' and 'Oubliette' in lockedRooms):
           # unlock the Oubliette to the monster
           print("You unlocked a secret door!")
           removeLRoom(rooms[currentRoom]['south'])
@@ -208,6 +216,7 @@ while True:
           continue
         else:
           print("That did\'nt seem to unlock anything.")
+          repeat = False
           continue
       if (move[1] == 'crystal ball'):
         # Get the message from Nasa
@@ -219,12 +228,15 @@ while True:
       repeat = False
       #Describe what is seen with closer inspection
       describeNoticeable()
-    elif (move[1] in rooms[currentRoom]['items']):
-        print(f"I might be able to use this {move[1]} later, but there\'s nothing to inspect")
+
+    elif ('items' in rooms[currentRoom] and move[1] in rooms[currentRoom]['items']):
+        print(f"I might be able to use this {move[1]} later, but there\'s nothing to inspect.")
         repeat = False
     else:
-        print("That\'s not in here")
-        repeat = False
+      print("That\'s not in here")
+      repeat = False
+
+
 
   #if they type 'get' first
   if move[0] == 'get' :
@@ -262,14 +274,16 @@ while True:
   def removeLRoom(room):
     lockedRooms.pop(lockedRooms.index(room))
 
+
+
   def use():
     if move[1].lower() in inventory:
-    # check if door can be unlocked with move[1]
-    # if move[1] == 'key' and locked == True\
+      # check if door can be unlocked with move[1]
+      # if move[1] == 'key' and locked == True\
+      nextRoomSouth = rooms[currentRoom]['south']
       if move[1] == 'key':
         try:
           if (rooms[currentRoom]['south']):
-            nextRoomSouth = rooms[currentRoom]['south']
             if (move[1] =='key' and nextRoomSouth in lockedRooms):
               removeLRoom(nextRoomSouth)
               removeInv(move[1])
@@ -284,34 +298,37 @@ while True:
             nextRoomSouth = rooms[currentRoom]['south']
             if (move[1] == 'silver key' and nextRoomSouth in lockedRooms):
               print(f"A {move[1]} unlocked a door to the {nextRoomSouth}")
+              removeLRoom(nextRoomSouth)
+              removeInv(move[1])
             else:
               print(f"{move[1]} doesn\'t seem to be useful here")
         except:
             print("There aren\'t any locked doors in here")
+      # Letter      
       elif move[1].lower() == 'letter':
-        print(" To whom it may concern, \n the password to the safe is the name of \n the asteroid potentially about \n  to destoy the world.")
+        print("~~~~~~~~~~~~~~~~~~~~~~~")
+        print(" Dear Prince Herbert, \n the password to the safe is the name of \n the asteroid potentially about \n  to destoy the world.")
+        print("~~~~~~~~~~~~~~~~~~~~~~")
+        time.sleep(2)
+        print("(The letter dissolves into thin air...)")
         removeInv(move[1])
+      # Sword  
       elif move[1].lower() == 'sword':
         if (currentRoom == 'Oubliette' and 'Ancient Red Dragon' in livingMonsters):
           print("You want to duke it out, ok.")
           answer = input("You do see how sharp his claws are right?  (y / n): ")
-            if answer.lower() == 'y':
-              print("You slayed it! The path is clear now.")
-              livingMonsters.pop(livingMonsters.index('Ancient Red Dragon'))
-              repeat - False
-              continue
-            else:
-              repeat = False    
-              print("Think about your next move...!")
-              continue
+          if answer.lower() == 'y':
+            print("You slayed it! The path is clear now.")
+            livingMonsters.pop(livingMonsters.index('Ancient Red Dragon'))
+            removeLRoom(nextRoomSouth)
+            repeat = False
+          else:
+            repeat = False    
+            print("Think about your next move...!")
       elif (move[1] in inventory):
         print(f"I don\'t see how this {move[1]} would be useful now")
     else:
       print(f"You don\'t have a {move[1]}")
-
-
-
-  livingMonsters = ['Ancient Red Dragon']
 
 
       
